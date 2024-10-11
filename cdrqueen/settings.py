a90f7479647f9afbd3821cb17926d98ca65cea2c
ellydhore <ellydhoregabrylle.belleza@gmail.com>
2024-10-11 21:33:44 +0800

    home partially finished

--- cdrqueen/settings.py
+++ cdrqueen/settings.py
@@ -127,10 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "shared/static",
+    BASE_DIR / "home/static",
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
