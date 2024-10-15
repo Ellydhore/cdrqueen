961f32c3fa14e406fc6e8a71bbfbce64442528da
ellydhore <ellydhoregabrylle.belleza@gmail.com>
2024-10-15 19:04:27 +0800

    home now renders favorites and best sellers from database

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.foo_home, name='home')
+    path('', views.foo_home, name='home'),
 ]
--- productmanagement/models.py
+++ productmanagement/models.py
@@ -67,7 +67,11 @@ class Product(models.Model):
 # PRODUCT IMAGE
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image_url = models.URLField(max_length=200)
+    image_url_1 = models.URLField(max_length=200, default="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=")
+    image_url_2 = models.URLField(max_length=200, null=True, blank=True)
+    image_url_3 = models.URLField(max_length=200, null=True, blank=True)
+    image_url_4 = models.URLField(max_length=200, null=True, blank=True)
+    image_url_5 = models.URLField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return f"{self.product.name} Image"
