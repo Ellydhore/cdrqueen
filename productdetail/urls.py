2614747f6dfe5be424b69cbb4db68c4e6b15c055
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2024-10-16 17:02:28 +0800

    productdetail dynamic

--- productdetail/urls.py
+++ productdetail/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.foo_product_detail, name='product_detail')
+    path('product/<int:product_id>/', views.foo_product_detail, name='product_detail'),
 ]
--- productdetail/views.py
+++ productdetail/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from productmanagement.models import Product, ProductImage
 
 # Create your views here.
-def foo_product_detail(request):
-    return render(request, 'productdetail.html')
+def foo_product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    context = {
+        'product': product
+    }
+    return render(request, 'productdetail.html', context)
