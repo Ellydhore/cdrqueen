fe03b3a51037886ff5368c892d19123ebf63f2fc
Alexsus <alessandra.kisteria@gmail.com>
2024-10-16 18:39:33 +0800

    working signup and login | design not finalized
    -signup and login fully functional
    -needs to be redesigned according to planned design

--- userauth/views.py
+++ userauth/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-def login_page(request):
-    return render(request, 'login.html')
+def signup_page(request):  # register
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect('home')  # Redirect to a homepage or another page
+        else:
+            print(form.errors)
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', { "form": form })
 
-def signup_page(request):
-    return render(request, 'signup.html')
+
+def login_page(request): # Login
+    if request.method == "POST":
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', { "form": form})
+
+
