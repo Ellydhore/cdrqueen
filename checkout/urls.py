06dff024343907d6d52c6b63240f8f449fd2cd23
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2024-12-05 00:31:41 +0800

    done

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from . import views  # Ensure this imports views from the current app
 
 urlpatterns = [
     path('', views.checkout_view, name='checkout'),
-    path('payment/', views.payment_view, name='payment'), 
+    path('payment/', views.payment_view, name='payment'),
+    path('confirm_order/', views.confirm_order_view, name='confirm_order'),  # Use `views.confirm_order_view` here
 ]
